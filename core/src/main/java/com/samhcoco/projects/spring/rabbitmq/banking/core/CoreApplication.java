879509b15c3188bfa85157acdcee8581c788781a
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-01-06 10:40:19 +0000

    Project initialization

---
+++ core/src/main/java/com/samhcoco/projects/spring/rabbitmq/banking/core/CoreApplication.java
@@ -0,0 +1,13 @@
+package com.samhcoco.projects.spring.rabbitmq.banking.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
---
+++ debit/src/main/java/com/samhcoco/projects/spring/rabbitmq/banking/debit/DebitApplication.java
@@ -0,0 +1,13 @@
+package com.samhcoco.projects.spring.rabbitmq.banking.debit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebitApplication.class, args);
+    }
+
+}
